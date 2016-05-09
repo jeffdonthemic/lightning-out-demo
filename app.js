2432acaecd508d74ce15b68f93d7a5925299de43
jeffdonthemic <jdouglas@salesforce.com>
2016-05-09 10:39:25 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,80 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+// require nforce
+var nforce = require('nforce');
+// create the connection with the connected app
+var org = nforce.createConnection({
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  redirectUri: process.env.CALLBACK_URL,
+  mode: 'single'
+});
+// authenticate and return oauth token
+org.authenticate({
+  username: process.env.USERNAME,
+  password: process.env.PASSWORD+process.env.SECURITY_TOKEN
+}, function(err, resp){
+  if(!err) console.log('W00t! Logged into Salesforce successfully! Cached Token: ' + org.oauth.access_token);
+  if(err) console.log(err);
+});
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res, next) {
+  res.render('index', {
+    title: 'Jeff Lightning Out!',
+    lightningEndPointURI: process.env.LIGHTNING_URL,
+    authToken: org.oauth.access_token
+  });
+});
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
